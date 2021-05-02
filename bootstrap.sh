dcd7122939964a1439048763a6f9421d79854dcb
Vladimir Polyakov <v.k.polyakov@gmail.com>
2021-05-02 21:17:20 +0300

    folder structure has been changed

--- bootstrap.sh
+++ bootstrap.sh
@@ -12,5 +12,6 @@ ln -f -s "$HOME/dotfiles/mako" "$HOME/.config/mako"
 ln -f -s "$HOME/dotfiles/nvim" "$HOME/.config/nvim"
 ln -f -s "$HOME/dotfiles/gammastep" "$HOME/.config/gammastep"
 
+ln -f -s "$HOME/dotfiles/vimrc" "$HOME/.vimrc"
 ln -f -s "$HOME/dotfiles/vim" "$HOME/.vim"
 ln -f -s "$HOME/dotfiles/alacritty.yml" "$HOME/.alacritty.yml"
