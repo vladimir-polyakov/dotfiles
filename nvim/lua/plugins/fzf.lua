fdc3cf9c580bd611c653d73c59ffd19c9f331e13
Vladimir Polyakov <v.k.polyakov@gmail.com>
2022-06-26 12:43:03 +0300

    wip

--- nvim/lua/plugins/fzf.lua
+++ nvim/lua/plugins/fzf.lua
@@ -1,4 +1,7 @@
-local set = vim.keymap.set
+local g, set = vim.g, vim.keymap.set
+
+g.fzf_preview_window = {"right:50%", "ctrl-s"}
+g.fzf_buffer_jump = 1
 
 set("n", "<Leader>f", ":Files<cr>")
 set("n", "<Leader>w", ":Windows<cr>")
